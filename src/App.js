30dd56390750f818de1409694a364b024a90f79d
Anna Fulton <afulton7@gmail.com>
2019-07-10 10:39:51 -0400

    Create handleChange, handleSubmit, and searchMovies functions

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
-import React from 'react'
-import { map } from 'ramda'
-import './App.css'
+import React, {useState} from 'react';
+import {map} from 'ramda';
+import './App.css';
 
 function App() {
+  const [criteria, setCriteria] = useState('');
+
+  const handleChange = e => {
+    setCriteria(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    searchMovies(criteria);
+  };
+
+  const searchMovies = criteria => {
+    fetch(`https://movie-search.jrs.camp?q=${encodeURI(criteria)}`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(response => console.log(response));
+  };
+
+  const token =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNb3ZpZSBTZWFyY2giLCJpYXQiOjE0OTg5MDkwMDYsImV4cCI6MTU5MzYwMzQwNiwiYXVkIjoibW92aWUtc2VhcmNoLmpycy5jYW1wIiwic3ViIjoibW92aWUtc2VhcmNoIn0.-U--gAcHA3rmbVIXYN-3fnhC37FuQa4KrXpmZG3D0G8';
+
   return (
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column">
-            <h1 className="title">Movie Search</h1>
-            <form className="form">
-              <div className="field">
-                <label className="label">Name:</label>
-                <div className="control">
-                  <input type="text" className="input" />
+    <section className='section'>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column'>
+            <h1 className='title'>Movie Search</h1>
+            <form className='form' onSubmit={handleSubmit}>
+              <div className='field'>
+                <label className='label'>Name:</label>
+                <div className='control'>
+                  <input type='text' className='input' onChange={handleChange} />
                 </div>
-                <div className="help">Please enter the name of a movie</div>
+                <div className='help'>Please enter the name of a movie</div>
               </div>
-              <div className="field">
-                <button className="button is-primary">Search</button>
+              <div className='field'>
+                <button className='button is-primary'>Search</button>
               </div>
             </form>
           </div>
-          <div className="column">
-            <div className="box">
+          <div className='column'>
+            <div className='box'>
               <ul>{/* {map(li, results)} */}</ul>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
