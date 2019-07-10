1aff5e28d796140488f1d7b0677bf75cd3491602
Tyler Dillon <harrell_t@icloud.com>
2019-07-10 11:31:37 -0400

    adding form validation and button validation

--- src/App.js
+++ src/App.js
@@ -1,90 +1,100 @@
-import React, {useState} from 'react';
-import {map} from 'ramda';
-import './App.css';
+import React, { useState } from 'react'
+import { map } from 'ramda'
+import './App.css'
 
 function App() {
-  const [criteria, setCriteria] = useState('');
-  const [results, setResults] = useState([]);
-  const [valid, setValid] = useState(true);
+  const [criteria, setCriteria] = useState('')
+  const [results, setResults] = useState([])
+  const [valid, setValid] = useState(true)
+  const [buttonVal, setButtonVal] = useState(true)
 
   const handleChange = e => {
-    setCriteria(e.target.value);
-  };
+    setCriteria(e.target.value)
+    if (criteria === '') {
+      setButtonVal(true)
+    } else {
+      setButtonVal(false)
+    }
+  }
 
   const handleSubmit = e => {
-    e.preventDefault();
-    searchMovies(criteria).then(res => setResults(res.Search));
-  };
+    e.preventDefault()
+    searchMovies(criteria).then(res => setResults(res.Search))
+  }
 
   const searchMovies = criteria => {
     return fetch(`https://movie-search.jrs.camp?q=${encodeURI(criteria)}`, {
       headers: {
-        authorization: `Bearer ${token}`,
-      },
-    }).then(response => response.json());
-  };
+        authorization: `Bearer ${token}`
+      }
+    }).then(response => response.json())
+  }
 
   const token =
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNb3ZpZSBTZWFyY2giLCJpYXQiOjE0OTg5MDkwMDYsImV4cCI6MTU5MzYwMzQwNiwiYXVkIjoibW92aWUtc2VhcmNoLmpycy5jYW1wIiwic3ViIjoibW92aWUtc2VhcmNoIn0.-U--gAcHA3rmbVIXYN-3fnhC37FuQa4KrXpmZG3D0G8';
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNb3ZpZSBTZWFyY2giLCJpYXQiOjE0OTg5MDkwMDYsImV4cCI6MTU5MzYwMzQwNiwiYXVkIjoibW92aWUtc2VhcmNoLmpycy5jYW1wIiwic3ViIjoibW92aWUtc2VhcmNoIn0.-U--gAcHA3rmbVIXYN-3fnhC37FuQa4KrXpmZG3D0G8'
 
   const checkValid = () => {
     if (!valid) {
-      return 'is-danger';
+      return 'is-danger'
     } else {
-      return '';
+      return ''
     }
-  };
+  }
 
   const handleValidation = () => {
-    if (criteria.length < 1) {
-      setValid(false);
+    if (criteria === '') {
+      setValid(false)
+      setButtonVal(true)
     } else {
-      setValid(true);
+      setValid(true)
+      setButtonVal(false)
     }
-  };
+  }
 
   return (
-    <section className='section'>
-      <div className='container'>
-        <div className='columns'>
-          <div className='column'>
-            <h1 className='title'>Movie Search</h1>
-            <form className='form' onSubmit={handleSubmit}>
-              <div className='field'>
-                <label className='label'>Name:</label>
-                <div className='control'>
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h1 className="title">Movie Search</h1>
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="field">
+                <label className="label">Name:</label>
+                <div className="control">
                   <input
-                    type='text'
+                    type="text"
                     className={`input ${checkValid()}`}
                     onChange={handleChange}
                     onBlur={handleValidation}
                   />
                 </div>
-                <div className='help'>Please enter the name of a movie</div>
+                <div className="help">Please enter the name of a movie</div>
               </div>
-              <div className='field'>
-                <button className='button is-primary'>Search</button>
+              <div className="field">
+                <button disabled={buttonVal} className="button is-primary">
+                  Search
+                </button>
               </div>
             </form>
           </div>
-          <div className='column'>
-            <div className='box'>
+          <div className="column">
+            <div className="box">
               <ul>{map(li, results)}</ul>
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
 
 const li = movie => {
   return (
-    <li key={movie.imdbId}>
+    <li key={movie.imdbID}>
       {movie.Title}
-      <img src={movie.Poster} />
+      <img alt="movie poster" src={movie.Poster} />
     </li>
-  );
-};
+  )
+}
 
-export default App;
+export default App
